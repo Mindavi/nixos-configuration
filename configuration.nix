ed2ea0cebf88734a8a476332f6f37ac5f0020c96
Rick van Schijndel <rol3517@gmail.com>
2021-07-20 23:30:38 +0200

    hydra-ca: add new packages that are required

--- configuration.nix
+++ configuration.nix
@@ -1,13 +1,158 @@
 { config, pkgs, lib, ... }:
 
 let
+  CatalystPluginPrometheusTiny = pkgs.buildPerlPackage {
+    pname = "Catalyst-Plugin-PrometheusTiny";
+    version = "0.005";
+    src = pkgs.fetchurl {
+      url = "mirror://cpan/authors/id/S/SY/SYSPETE/Catalyst-Plugin-PrometheusTiny-0.005.tar.gz";
+      sha256 = "a42ef09efdc3053899ae007c41220d3ed7207582cc86e491b4f534539c992c5a";
+    };
+    buildInputs = with pkgs.perlPackages; [ HTTPMessage Plack SubOverride TestDeep ];
+    propagatedBuildInputs = with pkgs.perlPackages; [ CatalystRuntime Moose PrometheusTiny PrometheusTinyShared ];
+    meta = {
+      description = "Prometheus metrics for Catalyst";
+      license = with pkgs.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+  DataRandom = pkgs.buildPerlPackage {
+    pname = "Data-Random";
+    version = "0.13";
+    src = pkgs.fetchurl {
+      url = "mirror://cpan/authors/id/B/BA/BAREFOOT/Data-Random-0.13.tar.gz";
+      sha256 = "eb590184a8db28a7e49eab09e25f8650c33f1f668b6a472829de74a53256bfc0";
+    };
+    buildInputs = with pkgs.perlPackages; [ FileShareDirInstall TestMockTime ];
+    meta = {
+      description = "Perl module to generate random data";
+      license = with pkgs.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
+  HashSharedMem = pkgs.perlPackages.buildPerlModule {
+    pname = "Hash-SharedMem";
+    version = "0.005";
+    src = pkgs.fetchurl {
+      url = "mirror://cpan/authors/id/Z/ZE/ZEFRAM/Hash-SharedMem-0.005.tar.gz";
+      sha256 = "324776808602f7bdc44adaa937895365454029a926fa611f321c9bf6b940bb5e";
+    };
+    buildInputs = with pkgs.perlPackages; [ ScalarString ];
+    meta = {
+      description = "Efficient shared mutable hash";
+      license = with pkgs.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+  PrometheusTiny = pkgs.buildPerlPackage {
+    pname = "Prometheus-Tiny";
+    version = "0.007";
+    src = pkgs.fetchurl {
+      url = "mirror://cpan/authors/id/R/RO/ROBN/Prometheus-Tiny-0.007.tar.gz";
+      sha256 = "0ef8b226a2025cdde4df80129dd319aa29e884e653c17dc96f4823d985c028ec";
+    };
+    buildInputs = with pkgs.perlPackages; [ HTTPMessage Plack TestException ];
+    meta = {
+      homepage = "https://github.com/robn/Prometheus-Tiny";
+      description = "A tiny Prometheus client";
+      license = with pkgs.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+
+  PrometheusTinyShared = pkgs.buildPerlPackage {
+    pname = "Prometheus-Tiny-Shared";
+    version = "0.023";
+    src = pkgs.fetchurl {
+      url = "mirror://cpan/authors/id/R/RO/ROBN/Prometheus-Tiny-Shared-0.023.tar.gz";
+      sha256 = "7c2c72397be5d8e4839d1bf4033c1800f467f2509689673c6419df48794f2abe";
+    };
+    buildInputs = with pkgs.perlPackages; [ DataRandom HTTPMessage Plack TestDifferences TestException ];
+    propagatedBuildInputs = with pkgs.perlPackages; [ HashSharedMem JSONXS PrometheusTiny ];
+    meta = {
+      homepage = "https://github.com/robn/Prometheus-Tiny-Shared";
+      description = "A tiny Prometheus client with a shared database behind it";
+      license = with pkgs.lib.licenses; [ artistic1 gpl1Plus ];
+    };
+  };
+  customPerlDeps = pkgs.buildEnv {
+    name = "hydra-perl-deps-content-addressed";
+    paths = with pkgs.perlPackages; lib.closePropagation
+      [ ModulePluggable
+        CatalystActionREST
+        CatalystAuthenticationStoreDBIxClass
+        CatalystDevel
+        CatalystDispatchTypeRegex
+        CatalystPluginAccessLog
+        CatalystPluginAuthorizationRoles
+        CatalystPluginCaptcha
+        CatalystPluginPrometheusTiny
+        CatalystPluginSessionStateCookie
+        CatalystPluginSessionStoreFastMmap
+        CatalystPluginSmartURI
+        CatalystPluginStackTrace
+        CatalystRuntime
+        CatalystTraitForRequestProxyBase
+        CatalystViewDownload
+        CatalystViewJSON
+        CatalystViewTT
+        CatalystXScriptServerStarman
+        CatalystXRoleApplicator
+        CryptPassphrase
+        CryptPassphraseArgon2
+        CryptRandPasswd
+        DBDPg
+        DBDSQLite
+        DataDump
+        DateTime
+        DigestSHA1
+        EmailMIME
+        EmailSender
+        FileSlurp
+        IOCompress
+        IPCRun
+        JSON
+        JSONAny
+        JSONXS
+        LWP
+        LWPProtocolHttps
+        NetAmazonS3
+        NetPrometheus
+        NetStatsd
+        PadWalker
+        Readonly
+        PrometheusTiny
+        PrometheusTinyShared
+        SQLSplitStatement
+        SetScalar
+        Starman
+        StringCompareConstantTime
+        SysHostnameLong
+        TermSizeAny
+        TextDiff
+        TextTable
+        XMLSimple
+        YAML
+        pkgs.nixUnstable
+        pkgs.nixUnstable.perl-bindings
+        pkgs.git
+        pkgs.boehmgc
+      ];
+  };
   hydraForCa = pkgs.hydra-unstable.overrideAttrs (oldAttrs: rec {
+    version = "unstable-content-addressed";
     src = pkgs.fetchFromGitHub {
       owner = "regnat";
       repo = "hydra";
       rev = "a1b6e82bd07ebe783bbec65a56501a81c56c92ff";
       sha256 = "sha256-CompknZoxSd6g10Nr/9H/KkCgoMqFx+JRLwP+J2TVzo=";
     };
+    buildInputs =
+      with pkgs; [
+        makeWrapper autoconf automake libtool unzip nukeReferences sqlite libpqxx
+        top-git mercurial subversion breezy openssl bzip2 libxslt
+        customPerlDeps perl nixUnstable
+        postgresql # for running the tests
+        nlohmann_json
+        boost
+    ];
   });
 in
 {
