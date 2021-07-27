79c1d7ec002b6340419a71643e5a0ac679025405
Rick van Schijndel <rol3517@gmail.com>
2021-07-27 08:00:11 +0200

    sudo: properly enable insults

--- configuration.nix
+++ configuration.nix
@@ -320,6 +320,9 @@ in
   services.udev.packages = [ pkgs.rtl-sdr ];
 
   security.sudo = {
+    package = pkgs.sudo.override {
+      withInsults = true;
+    };
     execWheelOnly = true;
     extraConfig = ''
       Defaults insults
