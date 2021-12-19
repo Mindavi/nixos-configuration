48061461fbb2e7db05ae16b1ead5fc355edfed06
Rick van Schijndel <rol3517@gmail.com>
2021-12-19 23:06:43 +0100

    traefik: add initial dynamic configuration

--- machines/aqua/modules/traefik.nix
+++ machines/aqua/modules/traefik.nix
@@ -14,6 +14,17 @@
       api.dashboard = true;
       accesslog = true;
     };
+    dynamicConfigOptions = {
+      http = {
+        routers.api = {
+          entrypoints = "web";
+          rule = "Host(`localhost`) && (PathPrefix(`/dashboard`) || PathPrefix(`/api`))";
+          service = "api@internal";
+          middlewares = "internal-whitelist";
+        };
+        middlewares.internal-whitelist.ipwhitelist.sourcerange = "10.0.2.0/24";
+      };
+    };
   };
 
   networking.firewall.allowedTCPPorts = [ 80 443 ];
