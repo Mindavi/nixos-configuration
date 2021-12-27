9e2ad71512c3936e66b323ad6a6169da19e24dad
Rick van Schijndel <rol3517@gmail.com>
2021-12-27 22:11:42 +0100

    flake: use nixpkgs fork with updated hydra

--- flake.nix
+++ flake.nix
@@ -1,8 +1,9 @@
 {
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs-mindavi-new-hydra.url = "github:Mindavi/nixpkgs/hydra-update/2021-12-17";
 
-  outputs = { self, nixpkgs-unstable }: {
-    nixosConfigurations.nixos-asus = nixpkgs-unstable.lib.nixosSystem {
+  outputs = { self, nixpkgs-unstable, nixpkgs-mindavi-new-hydra }: {
+    nixosConfigurations.nixos-asus = nixpkgs-mindavi-new-hydra.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./machines/nixos-asus/default.nix
