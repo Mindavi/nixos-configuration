96ff7ed0c2a2cd53ee5556db5b2562d0f8460d02
Rick van Schijndel <rol3517@gmail.com>
2022-03-28 21:31:20 +0200

    flake: update for testing new hydra updates from lheckemann

--- flake.nix
+++ flake.nix
@@ -1,9 +1,10 @@
 {
   inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs-mindavi-new-hydra.url = "github:Mindavi/nixpkgs/hydra-update/2021-12-17";
+  #inputs.nixpkgs-mindavi-new-hydra.url = "github:Mindavi/nixpkgs/hydra-update/2021-12-17";
+  inputs.nixpkgs-mayflower-new-hydra.url = "github:mayflower/nixpkgs/hydra-updates";
 
-  outputs = { self, nixpkgs-unstable, nixpkgs-mindavi-new-hydra }: {
-    nixosConfigurations.nixos-asus = nixpkgs-mindavi-new-hydra.lib.nixosSystem {
+  outputs = { self, nixpkgs-unstable, nixpkgs-mayflower-new-hydra }: {
+    nixosConfigurations.nixos-asus = nixpkgs-mayflower-new-hydra.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./machines/nixos-asus/default.nix
