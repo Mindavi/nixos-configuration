4aec0f21462c1bbfea662c62b9cbc64c41adf8d4
Rick van Schijndel <rol3517@gmail.com>
2022-06-01 23:29:39 +0200

    flake: update and only track nixos-unstable

--- flake.nix
+++ flake.nix
@@ -1,15 +1,14 @@
 {
-  inputs.nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs-master.url = "github:NixOS/nixpkgs/master";
+  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = { self, nixpkgs-unstable, nixpkgs-master }: {
-    nixosConfigurations.nixos-asus = nixpkgs-master.lib.nixosSystem {
+  outputs = { self, nixos-unstable }: {
+    nixosConfigurations.nixos-asus = nixos-unstable.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./machines/nixos-asus/default.nix
       ];
     };
-    nixosConfigurations.aqua = nixpkgs-unstable.lib.nixosSystem {
+    nixosConfigurations.aqua = nixos-unstable.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
         ./machines/aqua/default.nix
