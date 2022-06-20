d7ae6e44ad91cca3ce0a2dfc052693b70a84346b
Rick van Schijndel <rol3517@gmail.com>
2022-06-20 20:43:52 +0200

    flake: add input for nixpkgs fork from NickCao for testing hydra

--- flake.nix
+++ flake.nix
@@ -1,7 +1,8 @@
 {
   inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixos-new-hydra.url = "github:NickCao/nixpkgs/hydra-bump";
 
-  outputs = { self, nixos-unstable }: {
+  outputs = { self, nixos-unstable, nixos-new-hydra }: {
     nixosConfigurations.nixos-asus = nixos-unstable.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
