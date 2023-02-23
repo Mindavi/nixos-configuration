69ecd4a379b15ed535f02d4635df9381001fc3b5
Rick van Schijndel <rol3517@gmail.com>
2023-02-23 20:59:44 +0100

    run-aqua-vm: passthrough port 3001

--- run-aqua-vm.sh
+++ run-aqua-vm.sh
@@ -6,5 +6,5 @@ set -euxo pipefail
 
 vm=$(nix build .\#nixosConfigurations.aqua.config.system.build.vm --json --no-link | jq -r .[0].outputs.out )
 echo using vm path $vm
-QEMU_NET_OPTS="hostfwd=tcp::8123-:8123,hostfwd=tcp::8000-:8000" "$vm/bin/run-aqua-vm"
+QEMU_NET_OPTS="hostfwd=tcp::8123-:8123,hostfwd=tcp::8000-:8000,hostfwd=tcp::3001-:3001" "$vm/bin/run-aqua-vm"
 
