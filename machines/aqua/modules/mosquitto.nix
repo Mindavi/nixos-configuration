033f77964b3c55cd4372e013693d59f47f28dded
Rick van Schijndel <rol3517@gmail.com>
2023-12-05 20:39:48 +0100

    aqua: mosquitto: allow anonymous access on localhost, for now

--- machines/aqua/modules/mosquitto.nix
+++ machines/aqua/modules/mosquitto.nix
@@ -6,18 +6,18 @@
     logType = [ "all" ];
     listeners = [
       {
+        address = "127.0.0.1";
         port = 1883;
+        # TODO(Mindavi): unsafe, but ok-ish if only allowed from localhost
+        omitPasswordAuth = true;
         acl = [
           "topic read public/#"
 
           "user monitor"
           "topic read #"
 
-          "user phone"
-          "topic read #"
-          "topic write sensor/+/+/control"
-
-          "user devicelist"
+          "user rtl"
+          "topic write rtl_433/aqua/#"
         ];
       }
     ];
