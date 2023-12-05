0c6b9eac60ded33164853f07bff83d31e0ab9646
Rick van Schijndel <rol3517@gmail.com>
2023-12-05 20:08:52 +0100

    aqua: rtl_433: be less verbose

--- machines/aqua/modules/rtl_433.nix
+++ machines/aqua/modules/rtl_433.nix
@@ -10,7 +10,7 @@
     description = "rtl_433 listener daemon";
     serviceConfig = {
       Type = "exec";
-      ExecStart = "${pkgs.rtl_433}/bin/rtl_433 -F json -vv";
+      ExecStart = "${pkgs.rtl_433}/bin/rtl_433 -F json";
       DynamicUser = "yes";
       SupplementaryGroups = [ "plugdev" ];
       Restart = "on-failure";
