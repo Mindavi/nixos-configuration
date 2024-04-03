bfbf97503c315b8c981f0f09860bb7c5249fcfef
Rick van Schijndel <rol3517@gmail.com>
2024-04-03 20:23:04 +0200

    nixos-asus: update audio config

--- machines/nixos-asus/default.nix
+++ machines/nixos-asus/default.nix
@@ -255,8 +255,13 @@
     };
   };
 
-  sound.enable = true;
-  hardware.pulseaudio.enable = true;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
 
   # e.g. platformio and element use this, so make sure this is enabled.
   security.unprivilegedUsernsClone = true;
