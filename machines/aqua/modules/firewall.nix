d726d77178a5e1dbc1eef8e589df2173b4971506
Rick van Schijndel <rol3517@gmail.com>
2024-05-27 22:40:25 +0200

    aqua: firewall: also allow VM subnet

--- machines/aqua/modules/firewall.nix
+++ machines/aqua/modules/firewall.nix
@@ -1,7 +1,7 @@
 { pkgs, config, ... }:
 let
-  subnet = "192.168.1.0/24";
-  #subnet = "10.0.2.0/24";
+  subnetReal = "192.168.1.0/24";
+  subnetVm = "10.0.2.0/24";
 in
 {
   # Make sure firewall is enabled.
@@ -14,19 +14,19 @@ in
 
   networking.firewall.extraCommands = ''
     # mdns, zeroconf, avahi
-    #iptables -A nixos-fw -p udp -m udp -s ${subnet} --dport 5353 -j nixos-fw-accept
+    #iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 5353 -j nixos-fw-accept
 
     # mosquitto (insecure)
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnet} --dport 1883 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 1883 -j nixos-fw-accept
     
     # samba
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnet} --dport 139 -j nixos-fw-accept
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnet} --dport 445 -j nixos-fw-accept
-    iptables -A nixos-fw -p udp -m udp -s ${subnet} --dport 137 -j nixos-fw-accept
-    iptables -A nixos-fw -p udp -m udp -s ${subnet} --dport 138 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 139 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 445 -j nixos-fw-accept
+    iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 137 -j nixos-fw-accept
+    iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 138 -j nixos-fw-accept
 
     # home assistant (FIXME: to be removed again)
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnet} --dport 8123 -j nixos-fw-accept
+    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 8123 -j nixos-fw-accept
   '';
 }
 
