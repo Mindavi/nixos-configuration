8a5f4555372ca1867c2eb450b1d61a3feb02371e
Rick van Schijndel <rol3517@gmail.com>
2024-07-09 21:25:38 +0200

    run-aqua-vm.sh: add debug logging and map some extra network things

--- run-aqua-vm.sh
+++ run-aqua-vm.sh
@@ -4,7 +4,7 @@ set -euxo pipefail
 
 # Note: make sure firewall is setup properly.
 
-vm=$(nix build .\#nixosConfigurations.aqua.config.system.build.vm --json --no-link | jq -r .[0].outputs.out )
+vm=$(nix build -L .\#nixosConfigurations.aqua.config.system.build.vm --json --no-link | jq -r .[0].outputs.out )
 echo using vm path $vm
-QEMU_NET_OPTS="hostfwd=tcp::8123-:8123,hostfwd=tcp::8000-:8000,hostfwd=tcp::3001-:3001" "$vm/bin/run-aqua-vm"
+QEMU_NET_OPTS="hostfwd=tcp::5000-:8123,hostfwd=tcp::8000-:8000,hostfwd=tcp::3001-:3001,hostfwd=tcp::1883-:1883" "$vm/bin/run-aqua-vm"
 
