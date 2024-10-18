eac6f5d7d930746589b9dfa84e2cf34eb4dcc2d3
Rick van Schijndel <rol3517@gmail.com>
2024-10-18 08:41:23 +0200

    nixos-asus: prometheus: make it easy to add more (toplevel) json queries for the hydra queue runner metrics

--- machines/nixos-asus/modules/prometheus.nix
+++ machines/nixos-asus/modules/prometheus.nix
@@ -13,21 +13,26 @@
         enable = true;
         port = 7979;
         listenAddress = "127.0.0.1";
-        configFile = pkgs.writeTextFile {
-          name = "prometheus-json-exporter.yaml";
+        configFile = lib.debug.traceVal (pkgs.writers.writeYAML "prometheus-json-exporter.yaml" (
           # Inspiration taken from example configs: https://github.com/prometheus-community/json_exporter/blob/master/examples/config.yml
           # And from an issue that explains how to use the ValueType format: https://github.com/prometheus-community/json_exporter/issues/217
-          text = lib.generators.toYAML {} {
-            modules.default.metrics = [
-              {
-                name = "hydra_queuerunnerstatus_nrQueuedBuilds";
-                path = "{ .nrQueuedBuilds }";
-                help = "Number of builds in the queue";
-                valuetype = "gauge";
-              }
-            ];
-          };
-        };
+          {
+            modules.default.metrics = lib.debug.traceVal (
+              lib.attrsets.attrValues (
+                lib.attrsets.mapAttrs
+                  (name: value: {
+                    name = "hydra_queuerunnerstatus_${name}";
+                    path = "{ .${name} }";
+                    help = value;
+                    valuetype = "gauge";
+                  })
+                  {
+                    nrQueuedBuilds = "Number of builds in the queue";
+                    nrUnfinishedSteps = "Number of steps to be executed";
+                  }
+              )
+            );
+          }));
       };
       node = {
         enable = true;
