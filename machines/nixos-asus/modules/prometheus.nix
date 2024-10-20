11c351790cdae5c28799101c7d726c0d6c9122b6
Rick van Schijndel <rol3517@gmail.com>
2024-10-20 13:57:07 +0200

    nixos-asus: prometheus: removing tracing

--- machines/nixos-asus/modules/prometheus.nix
+++ machines/nixos-asus/modules/prometheus.nix
@@ -13,26 +13,24 @@
         enable = true;
         port = 7979;
         listenAddress = "127.0.0.1";
-        configFile = lib.debug.traceVal (pkgs.writers.writeYAML "prometheus-json-exporter.yaml" (
+        configFile = pkgs.writers.writeYAML "prometheus-json-exporter.yaml" (
           # Inspiration taken from example configs: https://github.com/prometheus-community/json_exporter/blob/master/examples/config.yml
           # And from an issue that explains how to use the ValueType format: https://github.com/prometheus-community/json_exporter/issues/217
           {
-            modules.default.metrics = lib.debug.traceVal (
-              lib.attrsets.attrValues (
-                lib.attrsets.mapAttrs
-                  (name: value: {
-                    name = "hydra_queuerunnerstatus_${name}";
-                    path = "{ .${name} }";
-                    help = value;
-                    valuetype = "gauge";
-                  })
-                  {
-                    nrQueuedBuilds = "Number of builds in the queue";
-                    nrUnfinishedSteps = "Number of steps to be executed";
-                  }
-              )
+            modules.default.metrics = lib.attrsets.attrValues (
+              lib.attrsets.mapAttrs
+                (name: value: {
+                  name = "hydra_queuerunnerstatus_${name}";
+                  path = "{ .${name} }";
+                  help = value;
+                  valuetype = "gauge";
+                })
+                {
+                  nrQueuedBuilds = "Number of builds in the queue";
+                  nrUnfinishedSteps = "Number of steps to be executed";
+                }
             );
-          }));
+          });
       };
       node = {
         enable = true;
