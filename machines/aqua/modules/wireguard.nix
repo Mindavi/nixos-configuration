335e0b2b97ab129482e58f22644aa5ee136632f2
Rick van Schijndel <rol3517@gmail.com>
2024-10-22 22:09:05 +0200

    aqua: update wireguard config

--- machines/aqua/modules/wireguard.nix
+++ machines/aqua/modules/wireguard.nix
@@ -8,16 +8,25 @@
   environment.systemPackages = with pkgs; [
     wireguard-tools
   ];
-  systemd.network.netdevs.wg0.wireguardPeers = [
-    {
-      wireguardPeerConfig = {
-        AllowedIPs = [
-          "172.16.1.1/32"
+  networking.firewall.allowedUDPPorts = [ 51820 ];
+  networking.wireguard = {
+    dynamicEndpointRefreshSeconds = 5;
+    interfaces = {
+      wg0 = {
+        ips = [ "172.16.1.8/24" ];
+        listenPort = 51820;
+        privateKeyFile = "/etc/nixos/secrets/wireguard_key";
+        generatePrivateKeyFile = true;
+        peers = [
+          {
+            name = "phone-rick";
+            publicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vldO1Gc=";
+            persistentKeepAlive = 25;
+            allowedIPs = [ "172.16.1.1/32" ];
+            endpoint = "192.168.1.73";
+          }
         ];
-        EndPoint = "192.168.1.80:51820";
-        PersistentKeepAlive = 30;
-        PublicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vldO1Gc=";
       };
-    }
-  ];
-};
+    };
+  };
+}
