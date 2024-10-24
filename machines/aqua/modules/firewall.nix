8a1d243248df68c24f7a8189debbedba35e49553
Rick van Schijndel <rol3517@gmail.com>
2024-10-24 23:44:29 +0200

    aqua: complete migration to nftables

--- machines/aqua/modules/firewall.nix
+++ machines/aqua/modules/firewall.nix
@@ -20,23 +20,10 @@ in
   # Open up 8000 for testing purposes.
   networking.firewall.allowedTCPPorts = [ 8000 ];
 
-  networking.firewall.extraCommands = lib.optionalString (!config.networking.nftables.enable) ''
-    # mdns, zeroconf, avahi
-    #iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 5353 -j nixos-fw-accept
+  networking.firewall.extraInputRules = ''
+    # mdns, zeroconf, avahi TODO(mindavi): probably remove?
+    #ip saddr { ${subnetReal}, ${subnetVm}, ${subnetWireGuard} } udp 5353 accept
 
-    # mosquitto (insecure)
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 1883 -j nixos-fw-accept
-
-    # samba
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 139 -j nixos-fw-accept
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 445 -j nixos-fw-accept
-    iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 137 -j nixos-fw-accept
-    iptables -A nixos-fw -p udp -m udp -s ${subnetReal},${subnetVm} --dport 138 -j nixos-fw-accept
-
-    # home assistant (FIXME: to be removed again)
-    iptables -A nixos-fw -p tcp -m tcp -s ${subnetReal},${subnetVm} --dport 8123 -j nixos-fw-accept
-  '';
-  networking.firewall.extraInputRules = lib.optionalString (config.networking.nftables.enable) ''
     # mosquitto (insecure)
     ip saddr { ${subnetReal}, ${subnetVm}, ${subnetWireGuard} } tcp dport 1883 accept
 
