1839f25753e7a0ac70a973be3122216ae72a3493
Rick van Schijndel <rol3517@gmail.com>
2024-11-04 22:39:48 +0100

    aqua: homepage-dashboard: add some services

--- machines/aqua/modules/homepage-dashboard.nix
+++ machines/aqua/modules/homepage-dashboard.nix
@@ -10,27 +10,42 @@
     listenPort = 8082;
     # TODO(mindavi): Disable again
     openFirewall = true;
+    settings = {
+      startUrl = "/dashboard";
+    };
 
     bookmarks = [
       {
-        Developer = [
+        General = [
           {
-            Github = [
+            "Home Assistant" = [
               {
-                abbr = "GH";
-                href = "https://github.com/";
+                abbr = "HASS";
+                href = "http://aqua.local";
+              }
+            ];
+            "Hydra" = [
+              {
+                abbr = "HY";
+                href = "http://aqua.local/hydra";
+              }
+            ];
+            "Traefik" = [
+              {
+                abbr = "TR";
+                href = "http://traefik.aqua/dashboard/";
               }
             ];
           }
         ];
       }
       {
-        Entertainment = [
+        Laptop = [
           {
-            YouTube = [
+            Syncthing = [
               {
-                abbr = "YT";
-                href = "https://youtube.com/";
+                abbr = "ST";
+                href = "http://localhost:8384";
               }
             ];
           }
