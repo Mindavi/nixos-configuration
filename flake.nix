0aa8b46bc8cc8a14e723a6d09478240f3cb6e2c3
Rick van Schijndel <rol3517@gmail.com>
2024-11-24 16:05:38 +0100

    nixos-asus: enable zed-editor again and switch back to standard nixos-unstable branch
    Hydra on the hydra-meson branch is a bit too broken

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
-  #inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable-small";
-  inputs.nixos-unstable.url = "github:qowoz/nixpkgs/hydra-meson";
+  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+  #inputs.nixos-unstable.url = "github:qowoz/nixpkgs/hydra-meson";
   inputs.nixvim = {
     url = "github:nix-community/nixvim";
     inputs.nixpkgs.follows = "nixos-unstable";
