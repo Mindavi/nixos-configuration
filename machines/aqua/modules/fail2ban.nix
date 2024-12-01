7dd66d43b3d46f35eb696d05301ca814b4b14848
Rick van Schijndel <rol3517@gmail.com>
2024-12-01 21:30:10 +0100

    aqua: fail2ban: don't ban 172.16.1.0/24 IPs

--- machines/aqua/modules/fail2ban.nix
+++ machines/aqua/modules/fail2ban.nix
@@ -2,12 +2,14 @@
 let
   #subnet = "10.0.2.0/24";
   subnet = "192.168.2.0/24";
+  subnetWireguard = "172.16.1.0/24";
 in
 {
   services.fail2ban = {
     enable = true;
     ignoreIP = [
       subnet
+      subnetWireguard
     ];
   };
 }
