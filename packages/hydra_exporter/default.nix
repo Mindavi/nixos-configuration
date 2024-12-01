18bbd6e629884b380227544676a88b45edfa4ded
Rick van Schijndel <rol3517@gmail.com>
2024-12-01 21:29:38 +0100

    hydra_exporter: upgrade to latest version

--- packages/hydra_exporter/default.nix
+++ packages/hydra_exporter/default.nix
@@ -10,8 +10,8 @@ buildGoModule {
   src = fetchFromGitHub {
     owner = "helsinki-systems";
     repo = "hydra_exporter";
-    rev = "2ce8334933ff267a26288cefccb287178b86a335";
-    hash = "sha256-YdJEyHeuY2AibpV/SC+3f2B4anQqoDa3cqZ1lIjmS6k=";
+    rev = "a68d9bd5351c9502d4eacf38241ef7f5bd373a81";
+    hash = "sha256-YdJEyHeuY2AibpV/SC+3f2B4anQqoDb3cqZ1lIjmS6k=";
   };
   vendorHash = "sha256-Vfh/MZXDOGduWnuR8f80peKjlk68shBKHyrSheinaZQ=";
 }
