758425dfeefe4e3ea012d28c319677812a10eb55
Rick van Schijndel <rol3517@gmail.com>
2024-12-02 20:51:53 +0100

    aqua: dashboard: pages need to be underneath settings

--- machines/aqua/modules/dashboard.nix
+++ machines/aqua/modules/dashboard.nix
@@ -15,52 +15,51 @@ in
         port = "8082";
         host = "127.0.0.1";
       };
+      pages = [
+        {
+          type = "bookmarks";
+          groups = [
+            {
+              title = "aqua";
+              links = [
+                {
+                  title = "Home assistant";
+                  url = "http://${aqua_url}";
+                }
+                {
+                  title = "Hydra";
+                  url = "http://${aqua_url}/hydra";
+                }
+                {
+                  title = "Prometheus";
+                  url = "http://${nixos-asus_url}/prometheus";
+                }
+                {
+                  title = "Grafana";
+                  url = "http://${nixos-asus_url}/grafana";
+                }
+                {
+                  title = "Traefik";
+                  url = "http://traefik.aqua/dashboard/";
+                }
+              ];
+            }
+            {
+              title = "nixos-asus";
+              links = [
+                {
+                  title = "Syncthing";
+                  url = "http://${nixos-asus_url}:8384";
+                }
+                {
+                  title = "Prometheus";
+                  url = "http://${nixos-asus_url}:9090";
+                }
+              ];
+            }
+          ];
+        }
+      ];
     };
-
-    pages = [
-      {
-        type = "bookmarks";
-        groups = [
-          {
-            title = "aqua";
-            links = [
-              {
-                title = "Home assistant";
-                url = "http://${aqua_url}";
-              }
-              {
-                title = "Hydra";
-                url = "http://${aqua_url}/hydra";
-              }
-              {
-                title = "Prometheus";
-                url = "http://${nixos-asus_url}/prometheus";
-              }
-              {
-                title = "Grafana";
-                url = "http://${nixos-asus_url}/grafana";
-              }
-              {
-                title = "Traefik";
-                url = "http://traefik.aqua/dashboard/";
-              }
-            ];
-          }
-          {
-            title = "nixos-asus";
-            links = [
-              {
-                title = "Syncthing";
-                url = "http://${nixos-asus_url}:8384";
-              }
-              {
-                title = "Prometheus";
-                url = "http://${nixos-asus_url}:9090";
-              }
-            ];
-          }
-        ];
-      }
-    ];
   };
 }
