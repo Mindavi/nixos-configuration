8788527c560a6ced6c6ef8c25b3387b6a34cbbc1
Rick van Schijndel <rol3517@gmail.com>
2024-12-06 19:13:22 +0100

    nixos-asus: remove prometheus

--- machines/nixos-asus/default.nix
+++ machines/nixos-asus/default.nix
@@ -13,7 +13,6 @@
     ./modules/gaming.nix
     ./modules/nix.nix
     #./modules/nvidia.nix
-    ./modules/prometheus.nix
     ./modules/syncthing.nix
     ./modules/wireguard.nix
     ../../modules/hydra.nix
