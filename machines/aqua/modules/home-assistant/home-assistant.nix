fa2fa25bd00c7b5e233f237cb18f5a1ee904c0fd
Rick van Schijndel <rol3517@gmail.com>
2025-01-14 21:24:06 +0100

    aqua: home-assistant: remove daily backup and replace with ui-setup backups

--- machines/aqua/modules/home-assistant/home-assistant.nix
+++ machines/aqua/modules/home-assistant/home-assistant.nix
@@ -80,16 +80,6 @@
           };
           mode = "single";
         }
-        {
-          alias = "Backup Home Assistant every night at 3 AM";
-          trigger = {
-            platform = "time";
-            at = "03:00:00";
-          };
-          action = {
-            service = "backup.create";
-          };
-        }
       ];
       "automation ui" = "!include automations.yaml";
     };
