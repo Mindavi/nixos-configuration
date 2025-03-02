4d33cc93b9815a9bb83e9b9460902e4bf58b63f8
Rick van Schijndel <rol3517@gmail.com>
2025-03-02 21:14:06 +0100

    nixos-asus: jellyfin: update list for hardware acceleration

--- machines/nixos-asus/modules/jellyfin.nix
+++ machines/nixos-asus/modules/jellyfin.nix
@@ -19,13 +19,16 @@
     extraPackages = with pkgs; [
       intel-media-driver
       intel-vaapi-driver
-      vaapiVdpau
+      #intel-vdpau-driver
+      libvdpau-va-gl
+      #vaapiVdpau
       intel-compute-runtime # OpenCL filter support (hardware tonemapping and subtitle burn-in)
       # OpenCL support for intel CPUs before 12th gen
       # see: https://github.com/NixOS/nixpkgs/issues/356535
       intel-compute-runtime-legacy1
       vpl-gpu-rt # QSV on 11th gen or newer
       intel-media-sdk # QSV up to 11th gen
+      intel-media-driver
     ];
   };
 }
