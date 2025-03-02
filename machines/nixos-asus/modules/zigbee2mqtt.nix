2f7b3c8c0536f03028c48e6ae4b2f42f04ad548d
Rick van Schijndel <rol3517@gmail.com>
2025-03-02 20:56:35 +0100

    nixos-asus: upgrade to zigbee2mqtt v2

--- machines/nixos-asus/modules/zigbee2mqtt.nix
+++ machines/nixos-asus/modules/zigbee2mqtt.nix
@@ -8,8 +8,7 @@
 {
   services.zigbee2mqtt = {
     enable = true;
-    # TODO(Mindavi): wait for https://github.com/NixOS/nixpkgs/pull/371053 to propagate to nixos-unstable
-    package = pkgs.zigbee2mqtt; #_2;
+    package = pkgs.zigbee2mqtt_2;
     settings = {
       homeassistant = true;  # enable autodiscovery even though we won't run home assistant on nixos-asus
       permit_join = false;
