84e6c782e27e62882152da4ed5a44b659d986af6
Rick van Schijndel <rol3517@gmail.com>
2025-03-09 13:01:47 +0100

    nixos-asus: firewall: update for mosquitto and new IP subnet

--- machines/nixos-asus/modules/firewall.nix
+++ machines/nixos-asus/modules/firewall.nix
@@ -6,7 +6,8 @@
 }:
 
 let
-  subnetReal = "192.168.1.0/24";
+  subnetInternal1 = "192.168.1.0/24";
+  subnetInternal2 = "192.168.178.0/24";
   subnetWireGuard = "172.16.1.0/24";
 in
 {
@@ -24,8 +25,6 @@ in
     #5201
     5000
     8080
-
-    1883
   ];
   # 20595 for 0ad
   # 6567 for mindustry
@@ -45,10 +44,13 @@ in
 
     extraInputRules = ''
       # Open up 8000 for testing purposes. E.g. running development servers.
-      ip saddr { ${subnetReal}, ${subnetWireGuard} } tcp dport 8000 accept
+      ip saddr { ${subnetInternal1}, ${subnetInternal2}, ${subnetWireGuard} } tcp dport 8000 accept
 
       # prometheus node_exporter
       ip saddr { ${subnetWireGuard} } tcp dport ${toString config.services.prometheus.exporters.node.port} accept
+
+      # mosquitto
+      ip saddr { ${subnetInternal1}, ${subnetInternal2}, ${subnetWireGuard} } tcp dport 1884 accept
     '';
   };
 }
