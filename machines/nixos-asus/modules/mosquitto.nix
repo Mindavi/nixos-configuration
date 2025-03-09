0c987d6cf01d9dc7c7930add7d238a73cc79c905
Rick van Schijndel <rol3517@gmail.com>
2025-03-09 13:03:11 +0100

    nixos-asus: mosquitto: add service for internal network

--- machines/nixos-asus/modules/mosquitto.nix
+++ machines/nixos-asus/modules/mosquitto.nix
@@ -20,6 +20,7 @@
         settings = {
           allow_anonymous = true;
         };
+        # !!! WARNING: only for services running on the same machine.
         users = {
           zigbee2mqtt = {
             password = "zb2mqttpass_test";
@@ -41,16 +42,23 @@
             ];
             password = "home-assistant-1";
           };
-          open_mqtt_gateway = {
-           acl = [
-              "write #"
-            ];
-            password = "MyPasswordForOpenMqttGateway";
-          };
         };
         acl = [
           "topic readwrite #"
         ];
+      }
+      {
+        # Available on all interfaces
+        address = null;
+        port = 1884;
+        users = {
+          open_mqtt_gateway = {
+            acl = [
+              "write #";
+            ];
+            hashedPassword = "$7$101$0O71NK+NPfshX2EX$3Q62cfnr+3ytTKiN449UU9mtrExTJ6cjDRU5bLLRIrQAxSN9eJ+pZj7rjT0ViWLcutoueaPJvvKptKfZ5AW5tQ==";
+          };
+        };
       }
     ];
   };
