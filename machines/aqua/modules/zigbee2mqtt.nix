f2ec41c8632b39dcef94bc4d9cf158d3bdbca30c
Rick van Schijndel <rol3517@gmail.com>
2025-03-13 21:32:40 +0100

    aqua: zigbee2mqtt: add config

--- machines/aqua/modules/zigbee2mqtt.nix
+++ machines/aqua/modules/zigbee2mqtt.nix
@@ -1,29 +1,45 @@
 {
   lib,
   config,
+  pkgs,
   ...
 }:
 
 {
   services.zigbee2mqtt = {
-    enable = false;
+    enable = true;
+    package = pkgs.zigbee2mqtt_2;
     settings = {
       homeassistant = config.services.home-assistant.enable;
       permit_join = false;
       serial = {
-        port = "mdns://slzb-06";
+        # TODO(Mindavi): mDNS does not work at all, figure out why this is. Maybe systemd service restrictions from NixOS?
+        #port = "mdns://slzb-06";
+        port = "tcp://192.168.178.23:6638";
         adapter = "zstack";
       };
       frontend = {
         enabled = true;
-        # TODO(Mindavi): port?
-        # port = 1234;
-        # base_url = "/zigbee2mqtt";
+        port = 8081;
       };
       mqtt = {
         server = "mqtt://localhost:1883";
         client_id = "zigbee2mqtt_1";
+        user = "zigbee2mqtt";
+        password = "zb2mqttpass_test";
+        base_topic = "zigbee2mqtt";
+      };
+      advanced = {
+        log_level = "info";
+        log_output = [
+          "console"
+        ];
       };
     };
+  };
+  systemd.services.zigbee2mqtt.serviceConfig = {
+    # zigbee2mqtt stops when the adapter disconnects from the network / the laptop disconnects from the network
+    # it happily exits with exit code 0, so let's just always try to restart here
+    Restart = lib.mkForce "always";
   };
 }
