1c2a416543944ddb2e61fec4444b2286a11018f2
Rick van Schijndel <rol3517@gmail.com>
2025-03-15 20:45:57 +0100

    aqua: home-assistant: try to fix broken install

--- machines/aqua/modules/home-assistant/dashboard.nix
+++ machines/aqua/modules/home-assistant/dashboard.nix
@@ -1,6 +1,21 @@
 { ... }:
 {
   services.home-assistant = {
-    config.lovelace.mode = "storage";
+    config.lovelace.mode = "yaml";
+    lovelaceConfig = {
+      title = "Thuis";
+      views = [
+        {
+          title = "Aansturing";
+          cards = [
+            {
+              type = "light";
+              entity = "light.lamp_slaapkamer_rick";
+              name = "Slaapkamer Rick";
+            }
+          ];
+        }
+      ];
+    };
   };
 }
