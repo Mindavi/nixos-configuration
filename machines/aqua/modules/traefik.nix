39195e554f0fa70f6d358aadb43e6a0f41df13a0
Rick van Schijndel <rol3517@gmail.com>
2025-05-15 07:26:16 +0200

    aqua: traefik: improve dashboard endpoint further

--- machines/aqua/modules/traefik.nix
+++ machines/aqua/modules/traefik.nix
@@ -141,9 +141,9 @@ in
         };
 
         ### Dashboard
-        middlewares.dashboard-stripprefix.stripprefix.prefixes = "/dashboard/";
+        middlewares.dashboard-stripprefix.stripprefix.prefixes = "/dashboard";
         routers.dashboard = {
-          rule = "PathPrefix(`/dashboard/`) && !(Host(`traefik.aqua`) || Host(`traefik.localhost`))";
+          rule = "(Path(`/dashboard/`) || Path(`/dashboard`)) && !(Host(`traefik.aqua`) || Host(`traefik.localhost`))";
           service = "dashboard";
           middlewares = [
             "internal-allowlist"
