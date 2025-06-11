070a9176bf6df0d0058af4d01c2eef09f3f83873
Rick van Schijndel <rol3517@gmail.com>
2025-06-11 08:15:30 +0200

    nixos-asus: upgrade nix to latest version

--- machines/nixos-asus/modules/nix.nix
+++ machines/nixos-asus/modules/nix.nix
@@ -7,7 +7,7 @@
 
 {
   nix = {
-    package = pkgs.nixVersions.nix_2_28;
+    package = pkgs.nixVersions.nix_2_29;
     settings = {
       sandbox = true;
       # decrease max number of jobs to prevent highly-parallelizable jobs from context-switching too much
