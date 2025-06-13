16f23cb934c92275fcf35df1b6e37a20377ccb4d
Rick van Schijndel <rol3517@gmail.com>
2025-06-13 16:22:27 +0200

    samba: setup global section and move map to guest setting there

--- machines/aqua/modules/samba.nix
+++ machines/aqua/modules/samba.nix
@@ -5,13 +5,21 @@
     # only open firewall to internal network, see firewall.nix
     openFirewall = false;
     settings = {
+      global = {
+        "invalid users" = [
+          "root"
+        ];
+        "passwd program" = "/run/wrappers/bin/passwd %u";
+        security = "user";
+        "map to guest" = "Never";
+      };
       copydrive = {
         path = "/var/data/samba/copydrive";
         "read only" = false;
         browseable = "yes";
         "guest ok" = "no";
         comment = "Opslag voor gezin";
-        "map to guest" = "bad user";
+        "valid users" = "familie";
       };
     };
   };
@@ -24,6 +32,8 @@
     home = "/var/data/samba";
     group = "familie";
     extraGroups = [ ];
+    # TODO(Mindavi): for setting up the samba password, maybe use activation scripts?
+    # https://search.nixos.org/options?channel=unstable&show=system.activationScripts
     initialPassword = "familie";
   };
 }
