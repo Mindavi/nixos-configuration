88031ceb8dcccad356a535cf6f0bc34229587333
Rick van Schijndel <rol3517@gmail.com>
2025-07-02 21:35:10 +0200

    aqua: music-assistant: configure some restarts to allow music-assistant to recover on machine startup
    For some reason it always fails to get setup properly the first time around.
    This is a lazy fix to at least ensure no manual restarts are needed (hopefully).

--- machines/aqua/modules/music-assistant.nix
+++ machines/aqua/modules/music-assistant.nix
@@ -16,4 +16,13 @@
       "squeezelite"
     ];
   };
+  systemd.services.music-assistant.serviceConfig = {
+    # For some reason, music-assistant fails on first startup.
+    # To be done is figure out what the reason is and fix it better, but for now just allow a few restarts.
+    Restart = "on-failure";
+    RestartSec = 120;
+    RestartSteps = 5;
+    RestartMaxDelaySec = "15min";
+    RestartMode = "debug";
+  };
 }
