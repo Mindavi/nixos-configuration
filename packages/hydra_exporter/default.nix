089670f86ea0103b0cb8b33fdc114448a21c8423
Rick van Schijndel <rol3517@gmail.com>
2025-07-07 21:52:54 +0200

    Revert "hydra_exporter: switch to fork to add a missing metric"
    This reverts commit 0ceffb48a4537002531124719cd0aafe7f5182ae.
    
    Seems that the changes are not actually working, let's revert for now.

--- packages/hydra_exporter/default.nix
+++ packages/hydra_exporter/default.nix
@@ -6,12 +6,12 @@
 
 buildGoModule {
   pname = "hydra_exporter";
-  version = "unstable-2025-07-02";
+  version = "unstable-2024-11-22";
   src = fetchFromGitHub {
-    owner = "Mindavi";
+    owner = "helsinki-systems";
     repo = "hydra_exporter";
-    rev = "ab8bfa778431586eeab67b02b28132db1b3eb34e";
-    hash = "sha256-ZP4tM6RMU6W0NB5IUkrDTMLVuZR506ppVXxmyN5vJDI=";
+    rev = "a68d9bd5351c9502d4eacf38241ef7f5bd373a81";
+    hash = "sha256-3BVpvTVYC3J8OWY4MrQ3+jiMIk6FInMErzWin3BZL/Q=";
   };
   vendorHash = "sha256-Vfh/MZXDOGduWnuR8f80peKjlk68shBKHyrSheinaZQ=";
 }
