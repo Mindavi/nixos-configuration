745cf2f1db2de47391733c108ddbb25e36802e27
Rick van Schijndel <rol3517@gmail.com>
2025-11-13 00:42:57 +0100

    castle: initial.sh: some fixes

--- machines/castle/initial.sh
+++ machines/castle/initial.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+set -euxo pipefail
 nix --extra-experimental-features "nix-command flakes" run github:nix-community/disko/latest -- --mode destroy,format,mount machines/castle/modules/disko.nix --yes-wipe-all-disks
-nixos-install --flake .# castle
+nixos-install --flake .#castle
 
