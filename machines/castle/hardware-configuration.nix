75ab960a718c3d2fbe3c0080b61cddac43c2ba2e
Rick van Schijndel <rol3517@gmail.com>
2025-11-12 23:37:44 +0100

    castle: hardware-configuration.nix: copy over (by retyping) as starting point
    Might contain a typo, who knows. But a start.

--- machines/castle/hardware-configuration.nix
+++ machines/castle/hardware-configuration.nix
@@ -1,1 +1,19 @@
-{ }
+{ config, lib, pkgs, modulesPath, ... }:
+
+{
+  # TODO(Mindavi): regenerate on castle. This was copied manually by screen reading.
+  imports =
+    [ (modulesPath + "/installer/scan/not-detected.nix")
+    ];
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.kernelModules = [ ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.extraModulePackages = [ ];
+
+  networking.useDHCP = lib.mkDefault true;
+  networking.interfaces.enp2s0.useDHCP = lib.mkDefault true;
+
+  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+}
