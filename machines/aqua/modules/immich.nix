727f9e3ff9c40e39eb9909ebadf9d86c89bbfd3f
Rick van Schijndel <rol3517@gmail.com>
2025-11-12 21:35:02 +0100

    aqua: immich: disable database and redis
    Not sure if this will actually do anything useful like this, though.
    But immich is still disabled anyways.

--- machines/aqua/modules/immich.nix
+++ machines/aqua/modules/immich.nix
@@ -7,5 +7,14 @@
     group = "immich"; # TODO(Mindavi): consider users?
     # use traefik instead of opening firewall
     openFirewall = false;
+    # let immich use the default for mediaLocation?
+    database = {
+      enable = false;
+      # Explicitly disable since it uses stateVersion and assumes we then always have had immich installed.
+      enableVectors = false;
+    };
+    redis = {
+      enable = false;
+    };
   };
 }
