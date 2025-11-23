25ef8198c183c9eaaef0eb8c7b4f2bb4c7077de7
Rick van Schijndel <rol3517@gmail.com>
2025-11-23 21:19:29 +0100

    nixos-asus: update IP for wireguard aqua
    Hopefully fixes some slowness there.

--- machines/nixos-asus/modules/wireguard.nix
+++ machines/nixos-asus/modules/wireguard.nix
@@ -32,7 +32,7 @@
             # TODO(mindavi): add networking.wireguard.interfaces.<name>.peers.*.presharedKeyFile
             publicKey = "D23jZc9k02dKnamHUAagtZPwrPAD0W40YWC6Pp5yP00=";
             allowedIPs = [ "172.16.1.8/32" ];
-            endpoint = "45.83.240.221:51820";
+            endpoint = "192.168.1.8:51820";
             dynamicEndpointRefreshSeconds = 5;
             dynamicEndpointRefreshRestartSeconds = 60;
           }
