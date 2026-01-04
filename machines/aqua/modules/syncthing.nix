f4628cc7a35aa3fb87abfe0d5ca5662d62d2440a
Rick van Schijndel <rol3517@gmail.com>
2026-01-04 15:41:39 +0100

    aqua: syncthing: attempt to ensure persistence of settings
    Seems like otherwise device settings are removed every restart?

--- machines/aqua/modules/syncthing.nix
+++ machines/aqua/modules/syncthing.nix
@@ -10,17 +10,6 @@
     user = "rick";
     configDir = "/home/rick/.config/syncthing";
     dataDir = "/storage/documents/samba/rick_documents/";
-    guiAddress = "127.0.0.1:8384";
-    extraFlags = [ ];
-    settings = {
-      gui = {
-        insecureSkipHostcheck = true;
-      };
-      options = {
-        globalAnnounceEnabled = false;
-        relaysEnabled = false;
-        urAccepted = -1;
-      };
-    };
+    guiAddress = "0.0.0.0:8384";
   };
 }
