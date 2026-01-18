cc4f02b398ab06a58c14ac33b90f03d72905710f
Rick van Schijndel <rol3517@gmail.com>
2026-01-18 13:51:58 +0100

    aqua: wireguard: add ipv6 addresses

--- machines/aqua/modules/wireguard.nix
+++ machines/aqua/modules/wireguard.nix
@@ -13,7 +13,7 @@
     enable = true;
     interfaces = {
       wg0 = {
-        ips = [ "172.16.1.8/24" ];
+        ips = [ "172.16.1.8/24" "fd37:191a:d082:0555::1/128" ];
         listenPort = 51820;
         mtu = 1280;
         privateKeyFile = "/etc/nixos/secrets/wireguard_key";
@@ -22,7 +22,7 @@
           {
             name = "phone-rick";
             publicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vIdO1Gc=";
-            allowedIPs = [ "172.16.1.1/32" ];
+            allowedIPs = [ "172.16.1.1/32" "fd37:191a:d082:0555::25/128" ];
             endpoint = "192.168.1.9:51820";
             dynamicEndpointRefreshSeconds = 5;
             dynamicEndpointRefreshRestartSeconds = 60;
@@ -30,7 +30,7 @@
           {
             name = "nixos-asus";
             publicKey = "uKb3tIPQCTSdQBKvXjQFVT22gj6BHNveP3PSzq9gQBI=";
-            allowedIPs = [ "172.16.1.2/32" ];
+            allowedIPs = [ "172.16.1.2/32" "fd37:191a:d082:0555::2/128" ];
             endpoint = "192.168.1.174:51820";
             dynamicEndpointRefreshSeconds = 5;
             dynamicEndpointRefreshRestartSeconds = 60;
