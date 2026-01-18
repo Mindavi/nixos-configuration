8d5d696e486324fc3bf29bd6d13e9b2f1a995f58
Rick van Schijndel <rol3517@gmail.com>
2026-01-18 13:51:32 +0100

    nixos-asus: wireguard: add ipv6 addresses

--- machines/nixos-asus/modules/wireguard.nix
+++ machines/nixos-asus/modules/wireguard.nix
@@ -13,7 +13,7 @@
     enable = true;
     interfaces = {
       wg0 = {
-        ips = [ "172.16.1.2/24" ];
+        ips = [ "172.16.1.2/24" "fd37:191a:d082:0555::2/128" ];
         listenPort = 51820;
         mtu = 1280;
         privateKeyFile = "/etc/nixos/secrets/wireguard_key";
@@ -22,7 +22,7 @@
           {
             name = "phone-rick";
             publicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vIdO1Gc=";
-            allowedIPs = [ "172.16.1.1/32" ];
+            allowedIPs = [ "172.16.1.1/32" "fd37:191a:d082:0555::25/128" ];
             endpoint = "192.168.1.9:51820";
             dynamicEndpointRefreshSeconds = 5;
             dynamicEndpointRefreshRestartSeconds = 60;
@@ -31,7 +31,7 @@
             name = "aqua";
             # TODO(mindavi): add networking.wireguard.interfaces.<name>.peers.*.presharedKeyFile
             publicKey = "D23jZc9k02dKnamHUAagtZPwrPAD0W40YWC6Pp5yP00=";
-            allowedIPs = [ "172.16.1.8/32" ];
+            allowedIPs = [ "172.16.1.8/32" "fd37:191a:d082:0555::1/128" ];
             endpoint = "192.168.1.8:51820";
             dynamicEndpointRefreshSeconds = 5;
             dynamicEndpointRefreshRestartSeconds = 60;
