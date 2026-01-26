0994a827ca2d1c2d56fa42aa452fa358b4170eae
Rick van Schijndel <rol3517@gmail.com>
2026-01-26 22:55:55 +0100

    nixos-asus: nix: 2.32 -> 2.33

--- machines/nixos-asus/modules/nix.nix
+++ machines/nixos-asus/modules/nix.nix
@@ -7,7 +7,7 @@
 
 {
   nix = {
-    package = pkgs.nixVersions.nix_2_32;
+    package = pkgs.nixVersions.nix_2_33;
     settings = {
       sandbox = true;
       # decrease max number of jobs to prevent highly-parallelizable jobs from context-switching too much
