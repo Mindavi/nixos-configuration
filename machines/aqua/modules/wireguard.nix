16b42dc2fc7858fbf4ba948a5272c238b045301f
Rick van Schijndel <rol3517@gmail.com>
2026-02-04 21:05:46 +0100

    wireguard: less ipv4

--- machines/aqua/modules/wireguard.nix
+++ machines/aqua/modules/wireguard.nix
@@ -14,7 +14,6 @@
     interfaces = {
       wg0 = {
         ips = [
-          "172.16.1.8/24"
           "fd37:191a:d082:0555::1/128"
         ];
         listenPort = 51820;
@@ -26,7 +25,6 @@
             name = "phone-rick";
             publicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vIdO1Gc=";
             allowedIPs = [
-              "172.16.1.1/32"
               "fd37:191a:d082:0555::25/128"
             ];
             endpoint = "192.168.1.9:51820";
@@ -37,7 +35,6 @@
             name = "nixos-asus";
             publicKey = "uKb3tIPQCTSdQBKvXjQFVT22gj6BHNveP3PSzq9gQBI=";
             allowedIPs = [
-              "172.16.1.2/32"
               "fd37:191a:d082:0555::2/128"
             ];
             endpoint = "192.168.1.174:51820";
--- machines/nixos-asus/modules/wireguard.nix
+++ machines/nixos-asus/modules/wireguard.nix
@@ -14,7 +14,6 @@
     interfaces = {
       wg0 = {
         ips = [
-          "172.16.1.2/24"
           "fd37:191a:d082:0555::2/128"
         ];
         listenPort = 51820;
@@ -26,7 +25,6 @@
             name = "phone-rick";
             publicKey = "BgCzrwWKlyV+zz1LqkxeedKJOdgdnXQs+U4/vIdO1Gc=";
             allowedIPs = [
-              "172.16.1.1/32"
               "fd37:191a:d082:0555::25/128"
             ];
             endpoint = "192.168.1.9:51820";
@@ -38,7 +36,6 @@
             # TODO(mindavi): add networking.wireguard.interfaces.<name>.peers.*.presharedKeyFile
             publicKey = "D23jZc9k02dKnamHUAagtZPwrPAD0W40YWC6Pp5yP00=";
             allowedIPs = [
-              "172.16.1.8/32"
               "fd37:191a:d082:0555::1/128"
             ];
             endpoint = "192.168.1.8:51820";
