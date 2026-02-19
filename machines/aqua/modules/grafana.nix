ca8402309f71bf7c173739fb5dc28ce31ac53c51
Rick van Schijndel <rol3517@gmail.com>
2026-02-19 20:45:43 +0100

    grafana: replace hardcoded secret_key with our own secret
    Fallout from https://github.com/NixOS/nixpkgs/pull/484374

--- machines/aqua/modules/grafana.nix
+++ machines/aqua/modules/grafana.nix
@@ -5,11 +5,14 @@
 {
   services.grafana = {
     enable = true;
-    settings.server = {
-      http_port = 2342;
-      http_addr = "127.0.0.1";
-      serve_from_sub_path = true;
-      root_url = "%(protocol)s://%(domain)s:%(http_port)s/grafana";
+    settings = {
+      server = {
+        http_port = 2342;
+        http_addr = "127.0.0.1";
+        serve_from_sub_path = true;
+        root_url = "%(protocol)s://%(domain)s:%(http_port)s/grafana";
+      };
+      security.secret_key = "$__file{${config.sops.secrets."grafana/secret_key"}}";
     };
   };
 }
--- machines/aqua/modules/sops.nix
+++ machines/aqua/modules/sops.nix
@@ -3,6 +3,9 @@
 {
   sops.defaultSopsFile = ../../../secrets/aqua.yaml;
   sops.secrets = {
+    "grafana/secret_key" = {
+      owner = config.systemd.services.grafana.serviceConfig.User;
+    };
     "mosquitto/monitor" = {
       owner = config.systemd.services.mosquitto.serviceConfig.User;
       group = config.systemd.services.mosquitto.serviceConfig.Group;
