5add4e215bc7a7ad561f58c710ba58346a1a14db
Rick van Schijndel <rol3517@gmail.com>
2026-02-19 21:41:31 +0100

    aqua, castle: fail2ban: Wireguard -> WireGuard

--- machines/aqua/modules/fail2ban.nix
+++ machines/aqua/modules/fail2ban.nix
@@ -2,14 +2,14 @@
 let
   #subnet = "10.0.2.0/24";
   subnet = "192.168.2.0/24";
-  subnetWireguardIpv6 = "fd37:191a:d082:555::1/96";
+  subnetWireGuardIpv6 = "fd37:191a:d082:555::1/96";
 in
 {
   services.fail2ban = {
     enable = true;
     ignoreIP = [
       subnet
-      subnetWireguardIpv6
+      subnetWireGuardIpv6
     ];
   };
 }
--- machines/castle/modules/fail2ban.nix
+++ machines/castle/modules/fail2ban.nix
@@ -1,14 +1,14 @@
 { pkgs, config, ... }:
 let
   subnetLan = "192.168.178.0/24";
-  subnetWireguardIpv6 = "fd37:191a:d082:555::1/96";
+  subnetWireGuardIpv6 = "fd37:191a:d082:555::1/96";
 in
 {
   services.fail2ban = {
     enable = true;
     ignoreIP = [
       subnetLan
-      subnetWireguardIpv6
+      subnetWireGuardIpv6
     ];
   };
 }
