b0bf141826daf4a71dc49ea6f40a2628816503a7
Rick van Schijndel <rol3517@gmail.com>
2026-02-19 21:24:15 +0100

    aqua, castle: update fail2ban configs to wireguard ipv6 subnets

--- machines/castle/modules/fail2ban.nix
+++ machines/castle/modules/fail2ban.nix
@@ -1,14 +1,14 @@
 { pkgs, config, ... }:
 let
   subnetLan = "192.168.178.0/24";
-  subnetWireguard = "172.16.1.0/24";
+  subnetWireguardIpv6 = "fd37:191a:d082:555::1/96";
 in
 {
   services.fail2ban = {
     enable = true;
     ignoreIP = [
       subnetLan
-      subnetWireguard
+      subnetWireguardIpv6
     ];
   };
 }
