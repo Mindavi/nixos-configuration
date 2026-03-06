9eaf60963e0a9373014646cc710cbd9148fdcc3b
Rick van Schijndel <rol3517@gmail.com>
2026-03-06 22:16:36 +0100

    aqua: squeezelite: disable for now

--- machines/aqua/modules/squeezelite.nix
+++ machines/aqua/modules/squeezelite.nix
@@ -4,7 +4,15 @@
 }:
 
 {
+  # Enable this to be able to play music on the server AUX output.
   services.squeezelite = {
-    enable = true;
+    # Fails on startup with the following logs:
+    # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352240] test_open:281 playback open error: Host is down
+    # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352292] output_init_common:401 unable to open output device: pipewire
+    # ExtraArguments for now doesn't seem to make any differences.
+    # Possibly permissions issue?
+    enable = false;
+    # TODO(Mindavi): get this also working with the Fiio BTR3K DAC.
+    extraArguments = "-o pipewire";
   };
 }
