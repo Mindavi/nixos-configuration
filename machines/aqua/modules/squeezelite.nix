ff3c32f4868d71d90c0009d77577419c6dbd04e9
Rick van Schijndel <rol3517@gmail.com>
2026-03-08 15:44:31 +0100

    aqua: squeezelite: fixes and hints
    Still broken, though.

--- machines/aqua/modules/squeezelite.nix
+++ machines/aqua/modules/squeezelite.nix
@@ -10,12 +10,13 @@
     # Fails on startup with the following logs:
     # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352240] test_open:281 playback open error: Host is down
     # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352292] output_init_common:401 unable to open output device: pipewire
+    # https://blog.masoko.net/linux/sqeezelite-wont-mix-with-user-audio/
     # ExtraArguments for now doesn't seem to make any differences.
     # Possibly permissions issue?
     enable = false;
     # TODO(Mindavi): get this also working with the Fiio BTR3K DAC.
-    extraArguments = "-o pipewire";
-    pulseAudio = true;
+    extraArguments = "-o default";
+    pulseAudio = false;
   };
-  config.systemd.services.squeezelite.serviceConfig.SupplementaryGroups = lib.mkForce [ "audio" "pipewire" ];
+  systemd.services.squeezelite.serviceConfig.SupplementaryGroups = lib.mkForce [ "audio" "pipewire" ];
 }
