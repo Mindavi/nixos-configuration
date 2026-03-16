e6d21d42c9bd3944f124d18e55ae904e9fe31c2f
Rick van Schijndel <rol3517@gmail.com>
2026-03-16 20:46:44 +0100

    immich: handle deprecations
    Was already setup correctly, now the options are removed.
    Nice cleanup.

--- machines/aqua/modules/immich.nix
+++ machines/aqua/modules/immich.nix
@@ -10,9 +10,6 @@
     # let immich use the default for mediaLocation?
     database = {
       enable = true;
-      # Explicitly disable since it uses stateVersion and assumes we then always have had immich installed.
-      enableVectors = false;
-      enableVectorChord = true;
     };
     redis = {
       enable = true;
