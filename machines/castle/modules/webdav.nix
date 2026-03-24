471470d0c2697492da669697713c1b99788ec2e4
Rick van Schijndel <rol3517@gmail.com>
2026-03-25 00:05:32 +0100

    castle: webdav: add tmpfiles rule to create /src/public folder

--- machines/castle/modules/webdav.nix
+++ machines/castle/modules/webdav.nix
@@ -15,10 +15,16 @@
         }
       ];
       debug = true;
+      # See warning below about systemd.tmpfiles.
       directory = "/srv/public";
       permissions = "CRUD";
     };
   };
   # TODO(Mindavi): route through traefik or setup more strict firewall rules.
   networking.firewall.allowedTCPPorts = [ 4918 ];
+
+  systemd.tmpfiles.rules = [
+    # Careful! This will remove the directory contents if it already exists.
+    "D ${config.services.webdav.settings.directory} 0755 webdav webdav"
+  ];
 }
