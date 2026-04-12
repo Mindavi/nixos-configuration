df767096d522ddc0d1d24c28430e0d680799e0eb
Rick van Schijndel <rol3517@gmail.com>
2026-04-12 21:05:46 +0200

    aqua: networking: move to separate file, add dhcpcd debug

--- machines/aqua/default.nix
+++ machines/aqua/default.nix
@@ -40,6 +40,7 @@ in
     ./modules/llm.nix
     ./modules/mosquitto.nix
     ./modules/music-assistant.nix
+    ./modules/networking.nix
     ./modules/nix.nix
     #./modules/owncast.nix
     ./modules/prometheus.nix
@@ -76,29 +77,6 @@ in
 
   hardware.cpu.intel.updateMicrocode = true;
   hardware.bluetooth.enable = true;
-
-  networking.hostName = "aqua";
-  # head -c 8 /etc/machine-id
-  networking.hostId = "c496aec3";
-  networking.networkmanager.enable = false;
-
-  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
-  networking.useDHCP = false;
-
-  networking.interfaces = lib.optionalAttrs (!isVmBuild) {
-    eno1 = {
-      # Intentionally enable both DHCP and static IP.
-      # This can be handy for recovery when network config is changed.
-      useDHCP = true;
-      ipv4.addresses = [
-        {
-          address = "192.168.1.8";
-          prefixLength = 24;
-        }
-      ];
-      # For IPv6 SLAAC and DHCPv6 are used. No need to define static IPs here.
-    };
-  };
 
   time.timeZone = "Europe/Amsterdam";
 
---
+++ machines/aqua/modules/networking.nix
@@ -0,0 +1,29 @@
+{ config, ... }:
+
+{
+  networking.hostName = "aqua";
+  # head -c 8 /etc/machine-id
+  networking.hostId = "c496aec3";
+  networking.networkmanager.enable = false;
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  networking.useDHCP = false;
+
+  networking.interfaces = lib.optionalAttrs (!isVmBuild) {
+    eno1 = {
+      # Intentionally enable both DHCP and static IP.
+      # This can be handy for recovery when network config is changed.
+      useDHCP = true;
+      ipv4.addresses = [
+        {
+          address = "192.168.1.8";
+          prefixLength = 24;
+        }
+      ];
+      # For IPv6 SLAAC and DHCPv6 are used. No need to define static IPs here.
+    };
+  };
+  dhcpcd.extraConfig = ''
+    debug
+  '';
+}
