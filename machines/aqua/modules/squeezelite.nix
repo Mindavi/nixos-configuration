a53d93c2d0f84d80b06763dd974ca2451997fbb2
Rick van Schijndel <rol3517@gmail.com>
2026-04-12 14:43:57 +0200

    aqua: squeezelite: remove comments about it being broken, it's fixed now
    Thanks to https://github.com/NixOS/nixpkgs/pull/506197.

--- machines/aqua/modules/squeezelite.nix
+++ machines/aqua/modules/squeezelite.nix
@@ -7,14 +7,7 @@
 {
   # Enable this to be able to play music on the server AUX output.
   services.squeezelite = {
-    # Fails on startup with the following logs:
-    # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352240] test_open:281 playback open error: Host is down
-    # Mar 06 22:07:13 aqua squeezelite[8447]: [22:07:13.352292] output_init_common:401 unable to open output device: pipewire
-    # https://blog.masoko.net/linux/sqeezelite-wont-mix-with-user-audio/
-    # ExtraArguments for now doesn't seem to make any differences.
-    # Possibly permissions issue?
     enable = true;
-    # TODO(Mindavi): get this also working with the Fiio BTR3K DAC.
     extraArgs = "-o default";
     pulseaudio.enable = true;
     name = "Kantoor-aqua";
