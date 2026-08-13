421f097c118d2dd4060b85b8ddfd52e319bf30bd
Rick van Schijndel <rol3517@gmail.com>
2026-08-14 01:09:34 +0200

    flake: go back to nixos-unstable channel

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   #inputs.nixos-unstable.url = "git:/home/rick/nixpkgs?ref=nixos-unstable";
-  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/b7c2ada94fe99c15b0dbcf4d11fd7850b957a436";
+  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
   #  inputs.nixvim = {
   #    url = "github:nix-community/nixvim";
   #    inputs.nixpkgs.follows = "nixos-unstable";
