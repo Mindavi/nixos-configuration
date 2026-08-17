8fe42cd68693bdaba92a304ef48bbd42873fbea0
Rick van Schijndel <rol3517@gmail.com>
2026-08-17 20:42:14 +0200

    aqua: move boot config to separate file

---
+++ machines/aqua/modules/boot.nix
@@ -0,0 +1,28 @@
+{
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot = {
+    enable = true;
+    # 2026-05-18: initrd 42MiB + bzImage 14MiB
+    configurationLimit = 7;
+    # Prevent getting a root shell by passing kernel parameters.
+    editor = false;
+    memtest86.enable = true;
+  };
+  boot.loader.efi.canTouchEfiVariables = false;
+  boot.kernelPackages = lib.mkDefault pkgs.linuxPackages_latest;
+  boot.kernelParams = [ ];
+  boot.kernel.sysctl = {
+    # https://www.kernel.org/doc/html/latest/mm/overcommit-accounting.html
+    "vm.overcommit_memory" = "0";
+  };
+
+  # The system will try to boot as far as possible even if mounting (or other services) fail.
+  # Otherwise it will be dropped in an emergency shell (without SSH access).
+  systemd.enableEmergencyMode = false;
+}
