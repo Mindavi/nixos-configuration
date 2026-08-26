c9c37c82e62618ccf525ccafaf5cc85e8f47b381
Rick van Schijndel <rol3517@gmail.com>
2026-08-26 20:54:41 +0200

    flake: unpin again

--- flake.nix
+++ flake.nix
@@ -1,7 +1,6 @@
 {
   #inputs.nixos-unstable.url = "git:/home/rick/nixpkgs?ref=nixos-unstable";
-  #inputs.nixos-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
-  inputs.nixos-unstable.url = "github:NixOS/nixpkgs/0e251e24a4f24e036a084b6b4b2d2491af4167f4";
+  inputs.nixos-unstable.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.zst";
   #  inputs.nixvim = {
   #    url = "github:nix-community/nixvim";
   #    inputs.nixpkgs.follows = "nixos-unstable";
